179e6b46c5382678ad73ae2ccba878c1066840ff
Alexander <sany199574@gmail.com>
2024-12-22 07:30:18 +0300

    Реалилизована сортировка пузырьком. Настроены тесты Добавлены дополнительные тесты для проверок

---
+++ src/SortLab/BumbleSort/BubbleSorter.cs
@@ -0,0 +1,26 @@
+﻿namespace BumbleSort
+{
+    public static class BubbleSorter
+    {
+        public static void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(array, j, j + 1);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int x, int y)
+        {
+            int temp = array[x];
+            array[x] = array[y];
+            array[y] = temp;
+        }
+    }
+}
---
+++ src/SortLab/SortingTests/SortingTests/BumbleSortTests.cs
@@ -0,0 +1,48 @@
+﻿using BumbleSort;
+
+namespace SortingTests.SortingTests
+{
+    public class BumbleSortTests
+    {
+        [Fact]
+        public void Sort_SortsArrayCorrectly()
+        {
+            // Arrange
+            int[] array = { 12, 5, 18, 1, 20, 7, 11, 15, 3, 16, 9, 6, 14, 2, 10, 13, 8, 17, 4, 19 };
+            int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            // Act
+            BubbleSorter.Sort(array);
+
+            // Assert
+            Assert.Equal(expected, array);
+        }
+
+        [Fact]
+        public void Sort_EmptyArray_NoException()
+        {
+            // Arrange
+            int[] array = Array.Empty<int>();
+
+            // Act
+            BubbleSorter.Sort(array);
+
+            // Assert
+            Assert.Empty(array);
+        }
+
+        [Fact]
+        public void Sort_SingleElementArray_NoChange()
+        {
+            // Arrange
+            int[] array = { 42 };
+
+            // Act
+            BubbleSorter.Sort(array);
+
+            // Assert
+            Assert.Single(array);
+            Assert.Equal(42, array[0]);
+        }
+    }
+}
