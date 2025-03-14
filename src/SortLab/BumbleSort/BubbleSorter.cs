6ed684c0bcfeeeebbf8ec19ded2ccc657fc1c45a
Alexander <sany199574@gmail.com>
2025-03-14 22:18:26 +0300

    Удалён метод  - Swap

--- src/SortLab/BumbleSort/BubbleSorter.cs
+++ src/SortLab/BumbleSort/BubbleSorter.cs
@@ -16,11 +16,6 @@
             }
         }
 
-        private static void Swap(int[] array, int x, int y)
-        {
-            int temp = array[x];
-            array[x] = array[y];
-            array[y] = temp;
-        }
+
     }
 }
