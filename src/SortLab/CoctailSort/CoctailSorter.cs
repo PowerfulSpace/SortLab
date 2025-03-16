5f779bc316cdf5c2a3f874c97ed1392a3cd99b89
Alexander <sany199574@gmail.com>
2025-03-16 23:50:58 +0300

    Добавлена реализация коктельной сортировки

--- src/SortLab/CoctailSort/CoctailSorter.cs
+++ src/SortLab/CoctailSort/CoctailSorter.cs
@@ -4,7 +4,49 @@
     {
         public static void Sort(int[] array)
         {
+            bool swapped = true;
+            int start = 0;
+            int end = array.Length - 1;
 
+            while (swapped)
+            {
+                swapped = false;
+
+                // Проход слева направо
+                for (int i = start; i < end; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        // Меняем элементы местами
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                // Если ничего не поменялось, массив уже отсортирован
+                if (!swapped)
+                    break;
+
+                swapped = false;
+                end--;
+
+                // Проход справа налево
+                for (int i = end - 1; i >= start; i--)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        // Меняем элементы местами
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                start++;
+            }
         }
 
         private static void Swap(int[] array, int x, int y)
