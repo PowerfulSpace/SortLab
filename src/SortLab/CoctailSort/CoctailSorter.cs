b166b80f7f9d32f81f30fec22377a4c526ac11ce
Alexander <sany199574@gmail.com>
2025-03-21 12:45:04 +0300

    Добавлен метод  InsertionSort

--- src/SortLab/CoctailSort/CoctailSorter.cs
+++ src/SortLab/CoctailSort/CoctailSorter.cs
@@ -46,6 +46,11 @@
             }
         }
 
-
+        private static void Swap(int[] array, int x, int y)
+        {
+            int temp = array[x];
+            array[x] = array[y];
+            array[y] = temp;
+        }
     }
 }
--- src/SortLab/InsertionSort/Class1.cs
+++ src/SortLab/InsertionSort/Class1.cs
@@ -2,6 +2,22 @@
 {
     public class Class1
     {
+        static void InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
 
+                // Сравниваем текущий элемент с элементами отсортированной части массива
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                array[j + 1] = key;
+            }
+        }
     }
 }
