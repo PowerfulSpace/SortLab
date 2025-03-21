8a4afc088ca0cce8860b88504dc7d4f8d4d9d898
Alexander <sany199574@gmail.com>
2025-03-21 12:45:41 +0300

    Убраны лишнии пробелы

--- src/SortLab/InsertionSort/Class1.cs
+++ src/SortLab/InsertionSort/Class1.cs
@@ -9,7 +9,6 @@
                 int key = array[i];
                 int j = i - 1;
 
-
                 while (j >= 0 && array[j] > key)
                 {
                     array[j + 1] = array[j];
