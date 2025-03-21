bae8fe7a01270ca8d0e54323b473d8b4ea70d4ad
Alexander <sany199574@gmail.com>
2025-03-21 04:58:04 +0300

    Удалён не испольхуемый метод

--- src/SortLab/CoctailSort/CoctailSorter.cs
+++ src/SortLab/CoctailSort/CoctailSorter.cs
@@ -46,11 +46,6 @@
             }
         }
 
-        private static void Swap(int[] array, int x, int y)
-        {
-            int temp = array[x];
-            array[x] = array[y];
-            array[y] = temp;
-        }
+
     }
 }
