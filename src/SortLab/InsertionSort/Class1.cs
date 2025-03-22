206357f227cd5d1bda786c259b62596592b3eaad
Alexander <sany199574@gmail.com>
2025-03-22 06:41:19 +0300

    Удалён код

--- src/SortLab/InsertionSort/Class1.cs
+++ src/SortLab/InsertionSort/Class1.cs
@@ -2,30 +2,6 @@
 {
     public class Class1
     {
-        static void InsertionSort(int[] array)
-        {
-            for (int i = 1; i < array.Length; i++)
-            {
-                int key = array[i];
-                int j = i - 1;
-
-                while (j >= 0 && array[j] > key)
-                {
-                    array[j + 1] = array[j];
-                    j--;
-                }
-
-                array[j + 1] = key;
-            }
-        }
-
-        static void PrintArray(int[] array)
-        {
-            foreach (int item in array)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-        }
+      
     }
 }
