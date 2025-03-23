f9d2c0c7385652cfed7708e9ad11975a6816546b
Alexander <sany199574@gmail.com>
2025-03-23 17:27:58 +0300

    Добавлена реализация сортировки  - SelectionSort

--- src/SortLab/SelectionSort/Class1.cs
+++ src/SortLab/SelectionSort/Class1.cs
@@ -3,5 +3,27 @@
     public class Class1
     {
 
+        static void SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                // Обмен элементов
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
     }
 }
