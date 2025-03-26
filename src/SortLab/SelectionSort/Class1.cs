a809f49ce35e8ceda9e17a74781fec276af7e9d4
Alexander <sany199574@gmail.com>
2025-03-26 05:16:10 +0300

    Переменованна переменная

--- src/SortLab/SelectionSort/Class1.cs
+++ src/SortLab/SelectionSort/Class1.cs
@@ -4,13 +4,13 @@
     {
         static void SelectionSort(int[] arr)
         {
-            int n = arr.Length;
+            int num = arr.Length;
 
-            for (int i = 0; i < n - 1; i++)
+            for (int i = 0; i < num - 1; i++)
             {
                 int minIndex = i;
 
-                for (int j = i + 1; j < n; j++)
+                for (int j = i + 1; j < num; j++)
                 {
                     if (arr[j] < arr[minIndex])
                     {
