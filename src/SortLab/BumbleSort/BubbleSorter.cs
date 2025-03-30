8893a713807bd2a529364c79bfd72fe76b521333
Alexander <sany199574@gmail.com>
2025-03-30 21:05:09 +0300

    Переписал алгоритм

--- src/SortLab/BumbleSort/BubbleSorter.cs
+++ src/SortLab/BumbleSort/BubbleSorter.cs
@@ -4,6 +4,17 @@
     {
         public static void Sort(int[] array)
         {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 1; j < array.Length - i; j++)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        Swap(array, j - 1, j);
+                    }
+
+                }                
+            }
         }
 
         private static void Swap(int[] array, int x, int y)
