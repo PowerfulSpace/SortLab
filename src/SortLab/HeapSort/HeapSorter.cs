553c40f7bd33bc084396cc2c7dc7f1cd39f06319
Alexander <sany199574@gmail.com>
2025-03-30 20:22:05 +0300

    Исправлены классы сортировок  -  добавлены необходимые методы

--- src/SortLab/HeapSort/HeapSorter.cs
+++ src/SortLab/HeapSort/HeapSorter.cs
@@ -2,5 +2,15 @@
 {
     public static class HeapSorter
     {
+        public static void Sort(int[] array)
+        {
+        }
+
+        private static void Swap(int[] array, int x, int y)
+        {
+            int temp = array[x];
+            array[x] = array[y];
+            array[y] = temp;
+        }
     }
 }
--- src/SortLab/ShellSort/ShellSorter.cs
+++ src/SortLab/ShellSort/ShellSorter.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShellSort
+﻿namespace ShellSort
 {
     public static class ShellSorter
     {
+        public static void Sort(int[] array)
+        {
+        }
+
+        private static void Swap(int[] array, int x, int y)
+        {
+            int temp = array[x];
+            array[x] = array[y];
+            array[y] = temp;
+        }
     }
 }
