c0585409d3148f723efa1d6aa82569f25967ff22
Alexander <sany199574@gmail.com>
2025-03-30 20:30:31 +0300

    Исправлены классы сортировок

--- src/SortLab/CoctailSort/Program.cs
+++ src/SortLab/CoctailSort/Program.cs
@@ -1,39 +1,12 @@
 ﻿
 
+using CoctailSort;
+
 int[] array = { 12, 5, 18, 1, 20, 7, 11, 15, 3, 16, 9, 6, 14, 2, 10, 13, 8, 17, 4, 19 };
 
 
-Sort(array);
+CoctailSorter.Sort(array);
 
 
 
 Console.ReadLine();
-
-
-void Sort(int[] array)
-{
-    for (int i = 0; i < array.Length / 2; i++)
-    {
-        for (int j = 0; j < array.Length - i - 1; j++)
-        {
-            if(array[j] > array[j + 1])
-            {
-                Swap(array, j, j + 1);
-            }
-        }
-        for (int k = array.Length - 1; k > 0 + i; k--)
-        {
-            if (array[k] < array[k - 1])
-            {
-                Swap(array, k, k - 1);
-            }
-        }
-    }
-}
-
-void Swap(int[] array, int x, int y)
-{
-    int temp = array[x];
-    array[x] = array[y];
-    array[y] = temp;
-}
