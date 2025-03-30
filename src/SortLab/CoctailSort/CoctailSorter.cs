efdadc4171a361a114ff4a44ecae3d758b5070b5
Alexander <sany199574@gmail.com>
2025-03-30 20:20:49 +0300

    Удалён алгоритм  - коктельная сортировка

--- src/SortLab/CoctailSort/CoctailSorter.cs
+++ src/SortLab/CoctailSort/CoctailSorter.cs
@@ -4,46 +4,7 @@
     {
         public static void Sort(int[] array)
         {
-            bool swapped = true;
-            int start = 0;
-            int end = array.Length - 1;
-
-            while (swapped)
-            {
-                swapped = false;
-
-                for (int i = start; i < end; i++)
-                {
-                    if (array[i] > array[i + 1])
-                    {
-
-                        int temp = array[i];
-                        array[i] = array[i + 1];
-                        array[i + 1] = temp;
-                        swapped = true;
-                    }
-                }
-
-                if (!swapped)
-                    break;
-
-                swapped = false;
-                end--;
-
-                for (int i = end - 1; i >= start; i--)
-                {
-                    if (array[i] > array[i + 1])
-                    {
-
-                        int temp = array[i];
-                        array[i] = array[i + 1];
-                        array[i + 1] = temp;
-                        swapped = true;
-                    }
-                }
-
-                start++;
-            }
+          
         }
 
         private static void Swap(int[] array, int x, int y)
