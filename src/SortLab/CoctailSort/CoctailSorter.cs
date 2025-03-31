38b7c16c907835789c7d8f1c21b23fdfb09a09a6
Alexander <sany199574@gmail.com>
2025-03-31 16:43:59 +0300

    Реализовал алгоритм  - CoctailSorter

--- src/SortLab/CoctailSort/CoctailSorter.cs
+++ src/SortLab/CoctailSort/CoctailSorter.cs
@@ -4,7 +4,39 @@
     {
         public static void Sort(int[] array)
         {
-          
+            int start = 0;
+            int end = array.Length - 1;
+
+            bool swaped = false;
+
+            while(start < end)
+            {
+                swaped = false;
+
+                for (int j = start; j < end; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(array, j, j + 1);
+                        swaped = true;
+                    }
+                }
+
+                end--;
+
+                for (int j = end; j > start; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        Swap(array, j - 1, j);
+                        swaped = true;
+                    }
+                }
+
+                start++;
+
+                if (!swaped) { return; }
+            }
         }
 
         private static void Swap(int[] array, int x, int y)
@@ -15,3 +47,6 @@
         }
     }
 }
+
+
+//улучшенная версия пузырьковой сортировки, которая проходит по массиву в обе стороны
