ff32cc8b36091e61f3ff17c7443bc84188cd29bc
Alexander <sany199574@gmail.com>
2025-04-01 16:42:53 +0300

    Оптимизировал алгоритм  - InsertionSorter

--- src/SortLab/InsertionSort/InsertionSorter.cs
+++ src/SortLab/InsertionSort/InsertionSorter.cs
@@ -6,16 +6,16 @@
         {
             for (int i = 1; i < array.Length; i++)
             {
-
                 int temp = array[i];
                 int j = i - 1;
 
                 while(j >= 0 && temp < array[j])
                 {
                     array[j + 1] = array[j];
-                    array[j] = temp;
                     j--;
                 }
+
+                array[j + 1] = temp;
             }
 
         }
