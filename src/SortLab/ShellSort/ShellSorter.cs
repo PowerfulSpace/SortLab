00597725e23b74be2570711fefbe84e06163b22f
Alexander <sany199574@gmail.com>
2025-04-02 20:51:18 +0300

    переписал алгоритм  - ShellSorter

--- src/SortLab/ShellSort/ShellSorter.cs
+++ src/SortLab/ShellSort/ShellSorter.cs
@@ -4,6 +4,27 @@
     {
         public static void Sort(int[] array)
         {
+            int gap = array.Length / 2;
+
+            while (gap >= 1)
+            {
+                for (int i = gap; i < array.Length; i++)
+                {
+                    int temp = array[i];
+                    int j = i;
+
+                    while (j >= gap && array[j - gap] > temp)
+                    {
+                        array[j] = array[j - gap];
+                        j -= gap;
+                    }
+
+                    array[j] = temp;
+                }
+
+                gap = gap / 2;
+            }
+
         }
 
         private static void Swap(int[] array, int x, int y)
