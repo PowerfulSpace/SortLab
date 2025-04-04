e9b539d00f37a10af44e424b257a5996724e81e7
Alexander <sany199574@gmail.com>
2025-04-04 21:15:37 +0300

    реализована сортировка  - BinaryTreeSorter

--- src/SortLab/BinaryTreeSort/BinaryTreeSorter.cs
+++ src/SortLab/BinaryTreeSort/BinaryTreeSorter.cs
@@ -1,9 +1,48 @@
 ﻿namespace BinaryTreeSort
 {
-    public static class BinaryTreeSorter
+    public static class BinaryTreeSorter<T> where T: IComparable<T>
     {
-        public static void Sort(int[] array)
+        public static void Sort(T[] array)
         {
+            Node<T>? root = null;
+
+            foreach (var value in array)
+            {
+                root = Insert(root, value);
+            }
+
+            List<T> sortedList = new List<T>();
+            InOrderTraversal(root, sortedList);
+
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                array[i] = sortedList[i];
+            }
+        }
+        private static Node<T> Insert<T>(Node<T>? node, T value)
+        {
+            if (node == null) return new Node<T>(value);
+
+            var comparer = Comparer<T>.Default;
+            if (comparer.Compare(value, node.value) < 0)
+            {
+                node.Left = Insert(node.Left, value);
+            }
+            else
+            {
+                node.Right = Insert(node.Right, value);
+            }
+
+            return node;
+        }
+
+        private static void InOrderTraversal<T>(Node<T>? node, List<T> result)
+        {
+            if (node == null) return;
+
+            InOrderTraversal(node.Left, result);
+            result.Add(node.value);
+            InOrderTraversal(node.Right, result);
         }
     }
 
--- src/SortLab/BinaryTreeSort/Program.cs
+++ src/SortLab/BinaryTreeSort/Program.cs
@@ -5,7 +5,7 @@ int[] array = { 12, 5, 18, 1, 20, 7, 11, 15, 3, 16, 9, 6, 14, 2, 10, 13, 8, 17, 
 
 Show(array);
 
-BinaryTreeSorter.Sort(array);
+BinaryTreeSorter<int>.Sort(array);
 
 Show(array);
 
