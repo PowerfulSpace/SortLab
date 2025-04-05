94dda3f6af39373a4194a8c98c5c37d648a3b69a
Alexander <sany199574@gmail.com>
2025-04-05 20:49:02 +0300

    Добавлен метод  - Show в HeapSort

--- src/SortLab/HeapSort/Program.cs
+++ src/SortLab/HeapSort/Program.cs
@@ -3,8 +3,21 @@ using HeapSort;
 
 int[] array = { 12, 5, 18, 1, 20, 7, 11, 15, 3, 16, 9, 6, 14, 2, 10, 13, 8, 17, 4, 19 };
 
+Show(array);
 
 HeapSorter.Sort(array);
 
+Show(array);
 
 Console.ReadLine();
+
+
+
+void Show(int[] array)
+{
+    foreach (int i in array)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
