6b575e44ed2376b81e082ccf56be3d1a22ca4b0d
Alexander <sany199574@gmail.com>
2025-04-08 00:00:06 +0300

    Удалил лишнюю проверку

--- src/SortLab/GnomeSort/Program.cs
+++ src/SortLab/GnomeSort/Program.cs
@@ -3,11 +3,9 @@ using GnomeSort;
 
 int[] array = { 12, 5, 18, 1, 20, 7, 11, 15, 3, 16, 9, 6, 14, 2, 10, 13, 8, 17, 4, 19 };
 
-Show(array);
 
 GnomeSorter.Sort(array);
 
-Show(array);
 
 Console.ReadLine();
 
