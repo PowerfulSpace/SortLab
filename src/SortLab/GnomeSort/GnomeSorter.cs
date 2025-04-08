7de52d77252047c99f40f5fb3a4946d7e88a3a11
Alexander <sany199574@gmail.com>
2025-04-09 02:11:31 +0300

    Реализован алгоритм  - GnomeSorter

--- src/SortLab/GnomeSort/GnomeSorter.cs
+++ src/SortLab/GnomeSort/GnomeSorter.cs
@@ -4,6 +4,20 @@
     {
         public static void Sort(int[] array)
         {
+            int index = 0;
+
+            while (index < array.Length - 1)
+            {
+                while(index >= 0 && array[index] > array[index + 1])
+                {
+                    Swap(array, index, index + 1);
+
+                    index--;
+                }
+
+                index++;
+            }
+
         }
 
         private static void Swap(int[] array, int x, int y)
@@ -14,3 +28,6 @@
         }
     }
 }
+
+
+//похожа на сортировку вставками, но с меньшим количеством шагов
