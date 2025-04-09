af6b4108be386e88cb19543393135b27a03d9b1a
Alexander <sany199574@gmail.com>
2025-04-09 23:35:29 +0300

    Добавлена реализация  - MergeSorter

--- src/SortLab/MergeSort/MergeSorter.cs
+++ src/SortLab/MergeSort/MergeSorter.cs
@@ -4,7 +4,24 @@
     {
         public static void Sort(int[] array)
         {
+            Separation(array.ToList());
         }
+
+        private static List<int> Separation(List<int> list)
+        {
+            if(list.Count == 1) { return list; }
+
+            List<int> left = Separation(list.GetRange(0, list.Count / 2));
+
+            //сортировка
+
+            List<int> rigth = Separation(list.GetRange(list.Count / 2, list.Count - list.Count / 2));
+
+            left.AddRange(rigth);
+
+            return left;
+        }
+
 
         private static void Swap(int[] array, int x, int y)
         {
