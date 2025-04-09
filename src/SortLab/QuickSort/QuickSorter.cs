16e5c0d983c10af5f2b83801e263ac61e23adcce
Alexander <sany199574@gmail.com>
2025-04-10 00:16:08 +0300

    Удаление лишних комментариев

--- src/SortLab/QuickSort/QuickSorter.cs
+++ src/SortLab/QuickSort/QuickSorter.cs
@@ -4,6 +4,36 @@
     {
         public static void Sort(int[] array)
         {
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        private static void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(array, low, high);
+
+                QuickSort(array, low, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, high);
+            }
+        }
+
+        private static int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, high);
+            return i + 1;
         }
 
         private static void Swap(int[] array, int x, int y)
