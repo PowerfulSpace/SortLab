c9aa129b58380779b0712b727f68644517ca9ae0
Alexander <sany199574@gmail.com>
2025-04-11 00:10:59 +0300

    Реализовал алгоритм  - CountingSorter

--- src/SortLab/CountingSort/CountingSorter.cs
+++ src/SortLab/CountingSort/CountingSorter.cs
@@ -4,13 +4,39 @@
     {
         public static void Sort(int[] array)
         {
-        }
+            {
+                // 1. Определяем минимальное и максимальное значения массива
+                int min = array[0];
+                int max = array[0];
+                foreach (int num in array)
+                {
+                    if (num < min) min = num;
+                    if (num > max) max = num;
+                }
 
-        private static void Swap(int[] array, int x, int y)
-        {
-            int temp = array[x];
-            array[x] = array[y];
-            array[y] = temp;
+                // 2. Создаём массив для подсчёта
+                int range = max - min + 1;
+                int[] count = new int[range];
+
+                // 3. Подсчитываем повторения каждого элемента
+                foreach (int num in array)
+                {
+                    count[num - min]++;
+                }
+
+                // 4. Перезаписываем элементы из count в исходный массив в отсортированном порядке
+                int index = 0;
+                for (int i = 0; i < range; i++)
+                {
+                    while (count[i] > 0)
+                    {
+                        array[index++] = i + min;
+                        count[i]--;
+                    }
+                }
+            }
         }
     }
 }
+
+//сортировка на основе подсчёта числа вхождений каждого значения в массив.
